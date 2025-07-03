7bac87143d1b71b62a2f906a01eb7749aa18d059
Roderick Lan <rodericklan03@gmail.com>
2025-07-02 18:59:36 -0600

    order class outline

---
+++ include/Order.hpp
@@ -0,0 +1,34 @@
+// Order.hpp
+// Order object
+#ifndef _ORDER_HPP
+#define _ORDER_HPP
+
+class Order { 
+public:
+  Order();
+  ~Order();
+
+  Order(const Order &);
+  Order operator=(const Order &);
+  
+
+private:
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
