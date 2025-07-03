41441c9e38857c5f8cf6e4e88672839189dd882f
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 16:56:34 -0600

    order documentation

--- include/Order.hpp
+++ include/Order.hpp
@@ -16,16 +16,44 @@ class Order {
 public:
   Order() = default;
   ~Order() = default;
-  Order(float _price, int _volume, const std::shared_ptr<Trader> &_maker)
-      : price{_price}, volume{_volume}, maker{_maker} {}
+  Order(float _price, int _volume, const std::shared_ptr<Trader> &_maker);
 
   Order(const Order &);
-  Order &operator=(const Order &);
+  Order &operator=(const Order &) = delete;
+  
+  // getters
+  /**
+   * @brief Get the bid/ask price of the order
+   * 
+   * @return float 
+   */
+  float get_price();
+  
+  /**
+   * @brief Get the volume of the trade. < 0 if short/sell, > 0 if long/buy
+   * 
+   * @return int 
+   */
+  int get_volume();
+  
+  /**
+   * @brief Get the maker object
+   * 
+   * @return Trader& 
+   */
+  Trader &get_maker();
 
+  /**
+   * @brief Execute order (partially or fully)
+   * 
+   * @return int volume traded
+   */
+  int make_trade();
 private:
   float price;
   int volume;
   const std::shared_ptr<Trader> maker;
+
 };
 
 #endif
