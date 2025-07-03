723e114ff0f287f3cd99932a4bba85e7f5fdf70c
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 17:02:45 -0600

    implemented value init for order

--- src/Order.cpp
+++ src/Order.cpp
@@ -2,3 +2,6 @@
 // Order class implementation
 
 #include "Order.hpp"
+
+Order::Order(float _price, int _volume, const std::shared_ptr<Trader> &_maker)
+    : price{_price}, volume{_volume}, maker{_maker} {}
