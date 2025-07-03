13fac9b70bd300b9ea495571b5d3e8c7dfcf86b7
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 15:52:39 -0600

    order book hpp

--- include/OrderBook.hpp
+++ include/OrderBook.hpp
@@ -1,7 +1,12 @@
 // OrderBook.hpp
 #ifndef _ORDER_BOOK_HPP
 #define _ORDER_BOOK_HPP
-
+#include <vector>
+#include "Order.hpp"
+struct OrderBook {
+  std::vector<Order> buy_orders;
+  std::vector<Order> sell_orders;
+};
 
 
 
