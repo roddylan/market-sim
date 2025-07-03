74e414752ce623d906da1db3a12d25b0d6044a2e
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 16:04:24 -0600

    orderbook destructors and constructor defns

--- include/OrderBook.hpp
+++ include/OrderBook.hpp
@@ -5,10 +5,18 @@
 #include <vector>
 
 struct OrderBook {
+  // default/empty constructor
   OrderBook() = default;
+  // destructor
+  ~OrderBook();
+  // value constructor
   OrderBook(const std::vector<Order> &_buys = {},
             const std::vector<Order> &_sells = {})
       : buy_orders(_buys), sell_orders(_sells) {}
+  // copy constructor
+  OrderBook(const OrderBook &);
+  // copy assignment
+  OrderBook &operator=(const OrderBook &);
 
   std::vector<Order> buy_orders;
   std::vector<Order> sell_orders;
