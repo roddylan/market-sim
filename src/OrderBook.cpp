cfc36a3e3f093b7030ca5269c24e6c929373ceba
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 18:40:04 -0600

    assignment operator for orderbook

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -9,3 +9,12 @@ OrderBook::OrderBook(const std::vector<Order> &_buys,
 
 OrderBook::OrderBook(const OrderBook &other)
     : buy_orders(other.buy_orders), sell_orders(other.sell_orders) {}
+
+OrderBook &OrderBook::operator=(const OrderBook &other) {
+  if (this == &other) {
+    return *this;
+  }
+  this->buy_orders = other.buy_orders;
+  this->sell_orders = other.sell_orders;
+  return *this;
+}
