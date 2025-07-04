eb3efdd8f61b09f2571dd00eba1c71a94472817d
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 21:05:50 -0600

    updated comparator to work with shared_ptr

--- include/OrderUtils.hpp
+++ include/OrderUtils.hpp
@@ -2,20 +2,21 @@
 #ifndef _ORDER_UTILS_HPP
 #define _ORDER_UTILS_HPP
 #include "Order.hpp"
+#include <memory>
 namespace OrderUtils {
 /**
  * @brief Comparator for Buy Orders
  *
  */
 struct BuyOrderCompare {
-  bool operator()(const Order &left, const Order &right) {
-    float l_price{left.get_price()}, r_price{right.get_price()};
+  bool operator()(const std::shared_ptr<Order> &left, const std::shared_ptr<Order> &right) {
+    float l_price{left->get_price()}, r_price{right->get_price()};
     // best price, highest bid
     if (l_price > r_price) {
       return true;
     }
     
-    auto l_time{left.get_time()}, r_time{right.get_time()};
+    auto l_time{left->get_time()}, r_time{right->get_time()};
     // earliest time first
     if (l_time < r_time) {
       return true;
@@ -25,14 +26,14 @@ struct BuyOrderCompare {
 };
 
 struct SellOrderCompare {
-  bool operator()(const Order &left, const Order &right) {
-    float l_price{left.get_price()}, r_price{right.get_price()};
+  bool operator()(const std::shared_ptr<Order> &left, const std::shared_ptr<Order> &right) {
+    float l_price{left->get_price()}, r_price{right->get_price()};
     // best price, lowest ask
     if (l_price < r_price) {
       return true;
     }
     
-    auto l_time{left.get_time()}, r_time{right.get_time()};
+    auto l_time{left->get_time()}, r_time{right->get_time()};
     // earliest first
     if (l_time < r_time) {
       return true;
