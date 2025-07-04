5f8f6f8c992a2b93fbef8819befaccec6fa67358
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 22:00:34 -0600

    clang format for matchingengine

--- include/MatchingEngine.hpp
+++ include/MatchingEngine.hpp
@@ -3,18 +3,17 @@
 #pragma once
 #include "Order.hpp"
 
-
 class MatchingEngine {
 public:
   /**
    * @brief Construct a new Matching Engine object
-   * 
+   *
    */
   MatchingEngine() = default;
-  
+
   /**
    * @brief Execute order (partially or fully)
-   * 
+   *
    * @param maker_order maker order
    * @param taker_order taker order
    * @return int volume traded
@@ -24,20 +23,19 @@ public:
 private:
   /**
    * @brief Execute sell order (maker selling, taker buying)
-   * 
-   * @param maker_order 
-   * @param taker_order 
+   *
+   * @param maker_order
+   * @param taker_order
    * @return int volume traded
    */
   int sell_order(const Order &maker_order, const Order &taker_order);
-  
+
   /**
    * @brief Execute buy order (maker buying, taker selling)
-   * 
-   * @param maker_order 
-   * @param taker_order 
+   *
+   * @param maker_order
+   * @param taker_order
    * @return int volume traded
    */
   int buy_order(const Order &maker_order, const Order &taker_order);
-
 };
