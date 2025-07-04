6cfec064bfb975fbfcc692a207fc90d096710d91
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 20:36:11 -0600

    clang formatting in orderbook and some matching logic with fair price

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -34,7 +34,9 @@ bool OrderBook::insert(const Order &order) {
 }
 
 float OrderBook::match(int req_vol, const float fair_price) {
-  if (req_vol == 0) { return 0; }
+  if (req_vol == 0) {
+    return 0;
+  }
   if (req_vol < 0) {
     return sell_match(req_vol, fair_price);
   } else {
@@ -42,17 +44,25 @@ float OrderBook::match(int req_vol, const float fair_price) {
   }
 }
 
-
 float OrderBook::buy_match(int req_vol, const float fair_price) {
   // match buy order to request
   // (maker buying, taker selling)
-  if (buy_orders.empty()) { return 0; }
+  if (buy_orders.empty()) {
+    return 0;
+  }
   const float buy_price = buy_orders.top().get_price();
-  const float sell_price = !sell_orders.empty() ? sell_orders.top().get_price() : buy_price;
+  const float sell_price =
+      !sell_orders.empty() ? sell_orders.top().get_price() : buy_price;
   const float base_price = (buy_price + sell_price) / 2;
 
   while (req_vol < 0) {
     // todo: dont pop, access order directly since only volume updating
+    float book_price = buy_orders.top().get_price();
+    if (fair_price > book_price) {
+      // taker wont want to sell
+      return 0;
+    }
+
     auto best_order = buy_orders.top();
     buy_orders.pop();
     req_vol -= best_order.make_trade(req_vol);
@@ -60,17 +70,20 @@ float OrderBook::buy_match(int req_vol, const float fair_price) {
       buy_orders.push(best_order);
     }
   }
-  
+
   return 0;
 }
 
 float OrderBook::sell_match(int req_vol, const float fair_price) {
   // match buy order to request
   // (maker buying, taker selling)
-  if (sell_orders.empty()) { return 0; }
+  if (sell_orders.empty()) {
+    return 0;
+  }
   const float sell_price = sell_orders.top().get_price();
-  const float buy_price = !buy_orders.empty() ? buy_orders.top().get_price() : sell_price;
+  const float buy_price =
+      !buy_orders.empty() ? buy_orders.top().get_price() : sell_price;
   const float base_price = (buy_price + sell_price) / 2;
-  
+
   return 0;
 }
