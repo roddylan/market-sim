d34e158c9ddba266a1a746fc84a39b736218cc5d
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 22:07:25 -0600

    placeholder returns

--- src/MatchingEngine.cpp
+++ src/MatchingEngine.cpp
@@ -18,3 +18,18 @@ int MatchingEngine::match_order(const Order &maker_order, const Order &taker_ord
 }
 
 
+int MatchingEngine::sell_order(const Order &maker_order, const Order &taker_order) {
+  // maker sell, taker buy
+  // taker vol > 0
+  assert(maker_order.get_volume() <= 0);
+  assert(taker_order.get_volume() >= 0);
+  return 0;
+}
+
+int MatchingEngine::buy_order(const Order &maker_order, const Order &taker_order) {
+  // maker buy, taker sell
+  // taker vol < 0
+  assert(maker_order.get_volume() >= 0);
+  assert(taker_order.get_volume() <= 0);
+  return 0;
+}
