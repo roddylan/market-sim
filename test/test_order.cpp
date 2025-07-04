28872d2d2dd9737ed5a1a594821244eac4a4d20e
Roderick Lan <rodericklan03@gmail.com>
2025-07-04 11:02:38 -0600

    started order test

--- test/test_order.cpp
+++ test/test_order.cpp
@@ -1,3 +1,14 @@
 // test_order.cpp
 // test orders 
 #include <gtest/gtest.h>
+#include "Order.hpp"
+#include "Trader.hpp"
+
+class OrderTest : public ::testing::Test {
+  void SetUp() override {
+    MMakerTrader maker("test");
+    MTakerTrader taker("test");
+    Order buy_order(49.5, 100, maker);
+    Order sell_order(50.5, -100, maker);
+  }
+};
