57266619b53ba659b7fdd7ba66b542b8ad1cb52d
Roderick Lan <rodericklan03@gmail.com>
2025-07-03 18:36:39 -0600

    orderbook copy constructor

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -2,7 +2,10 @@
 // OrderBook implementation
 
 #include "OrderBook.hpp"
-
+#include "Order.hpp"
 OrderBook::OrderBook(const std::vector<Order> &_buys,
                      const std::vector<Order> &_sells)
     : buy_orders(_buys), sell_orders(_sells) {}
+
+OrderBook::OrderBook(const OrderBook &other)
+    : buy_orders(other.buy_orders), sell_orders(other.sell_orders) {}
