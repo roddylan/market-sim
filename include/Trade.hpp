9caacd81467b12a7ad68b0857050ec54a6db2000
Roderick Lan <rodericklan03@gmail.com>
2025-07-05 12:36:59 -0600

    started trade struct

---
+++ include/Trade.hpp
@@ -0,0 +1,20 @@
+// Trade.hpp
+#pragma once
+#include "Trader.hpp"
+
+/**
+ * @brief Executed trade
+ * 
+ */
+struct Trade {
+  Trade() = delete;
+  ~Trade() = default;
+
+  Trade(const Trader *_maker, const Trader *_taker, float _price);
+  Trade(const Trade &other);
+  Trade &operator=(const Trade &) = delete;
+
+  const Trader* const maker;
+  const Trader* const taker;
+  const float price;
+};
