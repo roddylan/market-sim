ab6dbaec0cfbb1c165d80cf9bb8fcfa339e2a9ec
Roderick Lan <rodericklan03@gmail.com>
2025-07-05 17:22:28 -0600

    marketdata outline

--- include/MarketData.hpp
+++ include/MarketData.hpp
@@ -2,10 +2,19 @@
 // Market state info and other data
 #pragma once
 #include "Order.hpp"
+#include "Trade.hpp"
+#include <memory>
+#include <vector>
 
 class MarketData {
 public:
-
+  MarketData() = default;
+  ~MarketData() = default;
+  
+  Trade *get_last_trade();
+  float get_last_price();
 private:
-
+  // stack containing trade history
+  std::vector<std::unique_ptr<Trade>> trade_history;
+  std::vector<float> price_history;
 };
