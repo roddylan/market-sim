ab33089d0c8fdf5143bd34eb8431ef0ad93b63a1
Roderick Lan <rodericklan03@gmail.com>
2025-07-05 13:26:38 -0600

    ref constructor

--- include/Trade.hpp
+++ include/Trade.hpp
@@ -11,6 +11,7 @@ struct Trade {
   ~Trade() = default;
 
   Trade(const Trader *_maker, const Trader *_taker, float _price);
+  Trade(Trader &_maker, Trader &_taker, float _price);
   Trade(const Trade &other);
   Trade &operator=(const Trade &) = delete;
 
--- src/Trade.cpp
+++ src/Trade.cpp
@@ -5,5 +5,10 @@ Trade::Trade(const Trader *_maker, const Trader *_taker, float _price)
   assert(_price >= 0);
 }
 
+Trade::Trade(Trader &_maker, Trader &_taker, float _price)
+    : maker(&_maker), taker(&_taker), price(_price) {
+  assert(_price >= 0);
+}
+
 Trade::Trade(const Trade &other)
     : maker(other.maker), taker(other.taker), price(other.price) {}
