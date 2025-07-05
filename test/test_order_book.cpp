f12ad5588b19c88834a9f5204e11c302712cb8e6
Roderick Lan <rodericklan03@gmail.com>
2025-07-04 19:21:34 -0600

    started non empty order book test file

---
+++ test/test_order_book.cpp
@@ -0,0 +1,67 @@
+// test_order_book.cpp
+// Tests for order book
+
+#include <gtest/gtest.h>
+#include "OrderBook.hpp"
+#include "Order.hpp"
+#include "Trader.hpp"
+
+using Duration = std::chrono::nanoseconds;
+using Clock = std::chrono::steady_clock;
+using Time = std::chrono::time_point<std::chrono::steady_clock>;
+
+const float fair_price{100};
+const int default_abs_vol{100};
+const Time base_time = std::chrono::time_point<Clock>(Duration(10000));
+
+class OrderBookTest : public ::testing::Test {
+protected:
+  Trader *maker1;
+  Trader *maker2;
+  Trader *taker1;
+  Trader *taker2;
+  OrderBook book;
+
+
+  void SetUp() override {
+    maker1 = new MMakerTrader("1");
+    maker2 = new MMakerTrader("2");
+    taker1 = new MTakerTrader("1");
+    taker2 = new MTakerTrader("2");
+    book = OrderBook();
+    
+    Order buy_1(fair_price+1, 100, maker1, base_time);
+    Order buy_2(fair_price+0.5, 100, maker1, base_time);
+    Order buy_3(fair_price+1.5, 10, maker1, base_time);
+    Order buy_4(fair_price+1.5, 100, maker1, base_time - Duration(10));
+    Order buy_5(fair_price+1.5, 100, maker1, base_time + Duration(10));
+
+    Order sell_1(fair_price-1, -100, maker2, base_time);
+    Order sell_2(fair_price-0.5, -100, maker2, base_time);
+    Order sell_3(fair_price-1.5, -10, maker2, base_time);
+    Order sell_4(fair_price-1.5, -100, maker2, base_time - Duration(10));
+    Order sell_5(fair_price-1.5, -100, maker2, base_time + Duration(10));
+    
+    book.insert(buy_1);
+    book.insert(buy_2);
+    book.insert(buy_3);
+    book.insert(buy_4);
+    book.insert(buy_5);
+    book.insert(sell_1);
+    book.insert(sell_2);
+    book.insert(sell_3);
+    book.insert(sell_4);
+    book.insert(sell_5);
+
+    
+    
+  }
+
+  void TearDown() override {
+    delete maker1;
+    delete maker2;
+    delete taker1;
+    delete taker2;
+  }
+};
+
