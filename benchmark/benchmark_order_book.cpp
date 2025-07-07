5bf65ad7f04cff9e38059de8da1f6318b54a11f6
Roderick Lan <rodericklan03@gmail.com>
2025-07-07 14:16:15 -0600

    basic order book bench

--- benchmark/benchmark_order_book.cpp
+++ benchmark/benchmark_order_book.cpp
@@ -44,12 +44,57 @@ int main(int argc, char* argv[]) {
 
   for (int i = 0; i < n_orders; ++i) {
     taker_buy.push_back(
-      Order(price-i-1, abs_vol + 30, taker)
+      Order(price+i+1, abs_vol + 30, taker)
     );
     taker_sell.push_back(
-      Order(price+i+1, -abs_vol - 30, taker)
+      Order(price-i-1, -abs_vol - 30, taker)
     );
   }
+  using Clock = std::chrono::high_resolution_clock;
+
+  // pq
+  auto start_pq = Clock::now();
+  OrderBookPQ book_pq;
+  for (const Order &order : buy_orders) {
+    book_pq.insert(order);
+  }
+  for (const Order &order : sell_orders) {
+    book_pq.insert(order);
+  }
+  for (Order &order : taker_buy) {
+    book_pq.match(order);
+  }
+  for (Order &order : taker_sell) {
+    book_pq.match(order);
+  }
+  auto end_pq = Clock::now();
+
+  auto duration_pq = std::chrono::duration_cast<std::chrono::microseconds>(end_pq-start_pq);
+
+  std::cout << "ORDER_BOOK (PQ) | ELAPSED TIME = " << duration_pq.count() << " µs" << std::endl;
+
+  // pq
+  auto start_ms = Clock::now();
+  OrderBookPQ book_ms;
+  for (const Order &order : buy_orders) {
+    book_ms.insert(order);
+  }
+  for (const Order &order : sell_orders) {
+    book_ms.insert(order);
+  }
+  for (Order &order : taker_buy) {
+    book_ms.match(order);
+  }
+  for (Order &order : taker_sell) {
+    book_ms.match(order);
+  }
+  auto end_ms = Clock::now();
+
+  auto duration_ms = std::chrono::duration_cast<std::chrono::microseconds>(end_pq-start_pq);
+
+  std::cout << "ORDER_BOOK (MS) | ELAPSED TIME = " << duration_ms.count() << " µs" << std::endl;
+  
+
   
   
 }
