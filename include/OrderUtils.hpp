3a88c63d3e3511f772543166fb58634dc4b57e13
Roderick Lan <rodericklan03@gmail.com>
2025-07-07 11:14:26 -0600

    spacing fix

--- include/OrderUtils.hpp
+++ include/OrderUtils.hpp
@@ -3,38 +3,38 @@
 #include "Order.hpp"
 #include <memory>
 namespace OrderUtils {
-/**
- * @brief Comparator for Buy Orders
- *
- */
-struct BuyOrderCompare {
-  bool operator()(const std::shared_ptr<Order> &left, const std::shared_ptr<Order> &right) {
-    float l_price{left->get_price()}, r_price{right->get_price()};
-    // best price, highest bid
-    if (l_price != r_price) {
-      return l_price < r_price;
+  /**
+  * @brief Comparator for Buy Orders (priority queue)
+  *
+  */
+  struct BuyOrderCompare {
+    bool operator()(const std::shared_ptr<Order> &left, const std::shared_ptr<Order> &right) {
+      float l_price{left->get_price()}, r_price{right->get_price()};
+      // best price, highest bid
+      if (l_price != r_price) {
+        return l_price < r_price;
+      }
+      
+      // earliest time first
+      return left->get_time() > right->get_time();
     }
-    
-    // earliest time first
-    return left->get_time() > right->get_time();
-  }
-};
+  };
 
-/**
- * @brief Comparator for Sell Orders
- * 
- */
-struct SellOrderCompare {
-  bool operator()(const std::shared_ptr<Order> &left, const std::shared_ptr<Order> &right) {
-    float l_price{left->get_price()}, r_price{right->get_price()};
-    // best price, lowest ask
-    if (l_price != r_price) {
-      return l_price > r_price;
+  /**
+  * @brief Comparator for Sell Orders (priority queue)
+  * 
+  */
+  struct SellOrderCompare {
+    bool operator()(const std::shared_ptr<Order> &left, const std::shared_ptr<Order> &right) {
+      float l_price{left->get_price()}, r_price{right->get_price()};
+      // best price, lowest ask
+      if (l_price != r_price) {
+        return l_price > r_price;
+      }
+      
+      // earliest time first
+      return left->get_time() > right->get_time();
     }
-    
-    // earliest time first
-    return left->get_time() > right->get_time();
-  }
 };
 
 } // namespace OrderUtils
