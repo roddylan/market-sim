6e45294ac97895f4b917f03fe394d9acea062dd8
Roderick Lan <rodericklan03@gmail.com>
2025-07-07 23:03:31 -0600

    clang formatting, comments, get_trade_history

--- include/MarketData.hpp
+++ include/MarketData.hpp
@@ -7,33 +7,43 @@
 
 class MarketData {
 public:
+  using Trades = std::vector<std::unique_ptr<Trade>>;
+
   MarketData() = default;
   ~MarketData() = default;
-  
+
   /**
    * @brief Get the last trade (most recent)
-   * 
-   * @return Trade* 
+   *
+   * @return Trade*
    */
   Trade *get_last_trade() const;
-  
+
   /**
    * @brief Get the last price
-   * 
-   * @return float 
+   *
+   * @return float
    */
   float get_last_price() const;
 
   /**
+   * @brief Get the trade history object
+   *
+   * @return std::vector<std::unique_ptr<Trade>>
+   */
+  Trades get_trade_history() const;
+
+  /**
    * @brief add trade to history (and update price history)
-   * 
-   * @param maker 
-   * @param taker 
+   *
+   * @param maker
+   * @param taker
    * @param vwap volume weighted average price
    */
   void add_trade(Trader &maker, Trader &taker, float vwap);
+
 private:
   // stack containing trade history
-  std::vector<std::unique_ptr<Trade>> trade_history;
+  Trades trade_history;
   std::vector<float> price_history;
 };
--- src/MarketData.cpp
+++ src/MarketData.cpp
@@ -4,19 +4,24 @@
 #include <memory>
 
 Trade *MarketData::get_last_trade() const {
-  if (trade_history.empty()) { return nullptr; }
+  if (trade_history.empty()) {
+    return nullptr;
+  }
   return trade_history.back().get();
 }
 
 float MarketData::get_last_price() const {
-  if (price_history.empty()) { return 0; }
+  if (price_history.empty()) {
+    return 0;
+  }
   return price_history.back();
 }
 
+MarketData::Trades MarketData::get_trade_history() const {
+  return trade_history;
+}
 
 void MarketData::add_trade(Trader &maker, Trader &taker, float vwap) {
-  trade_history.push_back(
-    std::make_unique<Trade>(maker, taker, vwap)
-  );
+  trade_history.push_back(std::make_unique<Trade>(maker, taker, vwap));
   price_history.push_back(vwap);
 }
