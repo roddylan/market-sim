2648b1dd9d916f14fb186ba2bb99e0415c95d4c0
Roderick Lan <rodericklan03@gmail.com>
2025-07-07 23:32:27 -0600

    add trade test for market data

--- test/test_market_data.cpp
+++ test/test_market_data.cpp
@@ -5,7 +5,6 @@
 
 class MarketDataTest : public ::testing::Test {
 protected:
-  MarketData data;
   Trader *maker;
   Trader *taker;
   void SetUp() override {
@@ -22,8 +21,32 @@ protected:
 };
 
 TEST_F(MarketDataTest, TestEmpty) {
+  MarketData data;
   EXPECT_TRUE(data.get_trade_history().empty());
   EXPECT_FLOAT_EQ(data.get_last_price(), 0);
   EXPECT_EQ(data.get_last_trade(), nullptr);
   EXPECT_TRUE(data.get_prices().empty());
+}
+
+TEST_F(MarketDataTest, TestAddTrade) {
+  MarketData data;
+  EXPECT_TRUE(data.get_trade_history().empty());
+  EXPECT_FLOAT_EQ(data.get_last_price(), 0);
+  EXPECT_EQ(data.get_last_trade(), nullptr);
+  EXPECT_TRUE(data.get_prices().empty());
+  
+  data.add_trade(*maker, *taker, 150);
+  EXPECT_EQ(data.get_trade_history().size(), 1);
+  EXPECT_FLOAT_EQ(data.get_last_price(), 150);
+  EXPECT_NE(data.get_last_trade(), nullptr);
+  EXPECT_EQ(data.get_prices().size(), 1);
+
+  EXPECT_FLOAT_EQ(data.get_last_trade()->price, 150);
+  EXPECT_FLOAT_EQ(data.get_last_trade()->price, data.get_last_price());
+  EXPECT_EQ(data.get_last_trade()->maker, maker);
+  EXPECT_EQ(data.get_last_trade()->taker, taker);
+  
+  EXPECT_DEATH(data.add_trade(*maker, *taker, 0), ".*");
+  EXPECT_DEATH(data.add_trade(*maker, *taker, -1), ".*");
+
 }
