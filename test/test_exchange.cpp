41fffe67cf1bb84786bc05c335bc0de7caf61411
Roderick Lan <rodericklan03@gmail.com>
2025-07-07 22:57:50 -0600

    added price assertion for setter, price tests for exchange

--- test/test_exchange.cpp
+++ test/test_exchange.cpp
@@ -16,20 +16,27 @@ protected:
 
 
 TEST_F(ExchangeTest, DefaultInitialization) {
-  EXPECT_EQ(exchange.get_starting_price(), 100);
+  EXPECT_FLOAT_EQ(exchange.get_starting_price(), 100);
   Exchange ex1;
-  EXPECT_EQ(ex1.get_starting_price(), 100);
+  EXPECT_FLOAT_EQ(ex1.get_starting_price(), 100);
   EXPECT_EQ(ex1.get_book().get_buy_orders().size(), 0);
   EXPECT_EQ(ex1.get_book().get_sell_orders().size(), 0);
 }
 
 TEST_F(ExchangeTest, ValueInitialization) {
   Exchange ex1(150);
-  EXPECT_EQ(ex1.get_starting_price(), 150);
+  EXPECT_FLOAT_EQ(ex1.get_starting_price(), 150);
   EXPECT_EQ(ex1.get_book().get_buy_orders().size(), 0);
   EXPECT_EQ(ex1.get_book().get_sell_orders().size(), 0);
 
   EXPECT_DEATH(Exchange(0), ".*");
   EXPECT_DEATH(Exchange(-1), ".*");
+}
+
+
+TEST_F(ExchangeTest, StartingPrice) {
+  EXPECT_FLOAT_EQ(exchange.get_starting_price(), 100);
+  exchange.set_starting_price(1000);
+  EXPECT_FLOAT_EQ(exchange.get_starting_price(), 1000);
 }
 
